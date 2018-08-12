4bf2a321abca56fbf735fda31ae368c0a2f5d60d
loovjo <jonathan.loov@gmail.com>
2018-08-12 17:42:13 +0200

    Solid keeping state of being hidden

--- src/solid/mod.rs
+++ src/solid/mod.rs
@@ -6,19 +6,22 @@ use drawable::{Drawable, State, Position};
 
 
 pub struct Solid {
-    pub color: Color
+    pub color: Color,
+    shown: bool
 }
 
 impl Solid {
     pub fn new_sdl2(color: Color) -> Solid {
         Solid {
-            color
+            color,
+            shown: true,
         }
     }
 
     pub fn new_rgba(r: u8, g: u8, b: u8, a: u8) -> Solid {
         Solid {
-            color: Color::RGBA(r, g, b, a)
+            color: Color::RGBA(r, g, b, a),
+            shown: true,
         }
     }
 }
@@ -28,10 +31,15 @@ impl Drawable for Solid {
     fn content_mut(&mut self) -> Vec<&mut dyn Drawable> { vec![] }
 
     fn step(&mut self) {
+        self.shown = false;
     }
 
     fn state(&self) -> State {
-        State::Final
+        if self.shown {
+            State::Final
+        } else {
+            State::Hidden
+        }
     }
 
     fn draw(&mut self, canvas: &mut Canvas<Window>, position: &Position) {
